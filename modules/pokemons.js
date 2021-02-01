719958581520619e1be3584056e08288d5365003
nikolaichub <chub.mykola@ukr.net>
2021-02-01 23:32:22 +0200

    getPokemon() upgrated and now returns more pokemon's info

--- modules/pokemons.js
+++ modules/pokemons.js
@@ -1,26 +1,56 @@
-const Pokedex = require('pokedex-promise-v2');
+require('dotenv').config();
+// const Pokedex = require('pokedex-promise-v2');
+const fetch = require('node-fetch');
+const jsdom = require('jsdom');
 
-const api = new Pokedex();
+// const api = new Pokedex();
 
 // getting random pokemon id
 const getRandomPokemonId = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-// getting a Pokemon by name
-async function getPokemon(pokemonName) {
-  const response =
-    typeof pokemonName === 'string'
-      ? await api.getPokemonByName(pokemonName)
-      : await api.resource(`api/v2/pokemon/${pokemonName}`);
+// getting Pokemon's info list
+async function getPokemon(attr) {
+  const response = await fetch(process.env.POKEMON_INF0);
+  const data = await response.json();
+  const matches =
+    typeof attr === 'string'
+      ? data.filter((p) => p.slug === attr)
+      : data.filter((p) => p.id === attr);
+  const { length } = matches;
+  const info = matches[length - 1];
 
-  const pokemon = {
-    name: response.name,
-    id: response.id,
-    image: response.sprites.front_default,
-    types: response.types.map((type) => type.type.name).join(', '),
+  // getting a description from webpage
+  const pokeDesc = await fetch(`${process.env.POKEMON_DESC}${info.slug}`);
+  const html = await pokeDesc.text();
+  const parsedDoc = new jsdom.JSDOM(html);
+
+  return {
+    name: info.name,
+    id: info.id,
+    type: info.type.join(', '),
+    abilities: info.abilities.join(', '),
+    weakness: info.weakness.join(', '),
+    height: `${info.height} m`,
+    weight: `${info.weight} kg`,
+    image: info.ThumbnailImage,
+    description: parsedDoc.window.document
+      .querySelector('.version-x')
+      .textContent.replace(/\n/g, '')
+      .trim(),
   };
-
-  return pokemon;
 }
+
+// usage
+// const randomId = getRandomPokemonId(1, 898);
+// getPokemon(randomId)
+//   .then((data) => {
+//     console.log(data);
+//     process.exit(0);
+//   })
+//   .catch(() => {
+//     console.log('err');
+//     process.exit(0);
+//   });
 
 module.exports = {
   getPokemon,
