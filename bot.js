ddff74b4f1c0e0beccbc3ff95c9a9dcfd5cfed24
nikolaichub <chub.mykola@ukr.net>
2021-02-02 21:31:20 +0200

    getting particular pokemon

--- bot.js
+++ bot.js
@@ -4,36 +4,59 @@ const pokemon = require('./modules/pokemons');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-const createTemplate = (response) => `
-Name: ${response.name}
-Id: ${response.id}
-Types: ${response.type}
+// creates template from Pokemon's Info
+function createTemplate(response) {
+  return `
+${response.id} *${response.name}*
+Type: ${response.type}
+
+${response.description}
+
 Abilities: ${response.abilities}
 Weakness: ${response.weakness}
 Height: ${response.height}
-Weight: ${response.weight}
-Description: ${response.description}
-`;
+Weight: ${response.weight}`;
+}
+
+// getting Pokemon Info
+function getPokemom(ctx, attr, mess) {
+  return pokemon
+    .getPokemon(attr)
+    .then((response) => {
+      ctx.replyWithPhoto(
+        { url: response.image },
+        { caption: createTemplate(response), parse_mode: 'Markdown' }
+      );
+    })
+    .catch(() => {
+      ctx.reply(mess);
+    });
+}
 
 bot.start((ctx) => {
   ctx.reply('Welcome');
-  ctx.reply('Send me name of Pokémon', Markup.keyboard([['Get Random Pokemon']]).resize());
+  ctx.reply('Send me name of Pokémon', {
+    parse_mode: 'Markdown',
+    ...Markup.keyboard([['Get Random Pokemon']]).resize(),
+  });
 });
 
 bot.hears('Get Random Pokemon', (ctx) => {
-  const random = pokemon.getRandomPokemonId(1, 898);
-  pokemon
-    .getPokemon(random)
-    .then((response) => {
-      ctx.replyWithPhoto({ url: response.image }, { caption: createTemplate(response) });
-    })
-    .catch(() => {
-      ctx.reply('Something went wrong.. Try again!');
-    });
+  const randomId = pokemon.getRandomPokemonId(1, 898);
+  const errorMessage = 'Something went wrong.. Try again!';
+  getPokemom(ctx, randomId, errorMessage);
+});
+
+bot.on('text', (ctx) => {
+  const message = ctx.message.text;
+  const number = Number(message);
+  const type = number ? 'number' : 'string';
+  const attr = type === 'string' ? message.trim().toLowerCase() : number;
+  const errorMessage = 'Something went wrong or there is no such pokemon.. Try again!';
+  getPokemom(ctx, attr, errorMessage);
 });
 
 // bot.help((ctx) => ctx.reply('Send me a sticker'));
-// bot.on('sticker', (ctx) => ctx.reply('👍'));
 // bot.hears('hi', (ctx) => ctx.reply('Hey there'));
 
 bot.launch();
