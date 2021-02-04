d36d6983b396c72d141f64234556a4a15b1dc3c2
nikolaichub <chub.mykola@ukr.net>
2021-02-04 22:44:53 +0200

    Promise.all to speed up the response.

--- modules/pokemons.js
+++ modules/pokemons.js
@@ -37,60 +37,62 @@ async function getPokemonByType(filter) {
 
 // getting Pokemon's info list
 async function getPokemon(attr) {
-  const response = await fetch(process.env.POKEMON_INF0);
-  const data = await response.json();
-  const matches =
-    typeof attr === 'string'
-      ? data.filter((p) => p.slug === attr)
-      : data.filter((p) => p.id === attr);
-  const { length } = matches;
-  const info = matches[length - 1];
+  // getting not full info
+  async function getInfo(attr) {
+    const response = await fetch(process.env.POKEMON_INF0);
+    const data = await response.json();
+    const matches =
+      typeof attr === 'string'
+        ? data.filter((p) => p.slug === attr)
+        : data.filter((p) => p.id === attr);
+    const { length } = matches;
+    const info = matches[length - 1];
 
-  // getting a description from webpage
-  const pokeDesc = await fetch(`${process.env.POKEMON_DESC}${info.slug}`);
-  const htmlDesc = await pokeDesc.text();
-  const parsedDesc = new jsdom.JSDOM(htmlDesc);
+    return {
+      name: info.name,
+      id: info.id,
+      type: info.type.map((x) => `/${x}`).join(', '),
+      abilities: info.abilities.join(', '),
+      weakness: info.weakness.join(', '),
+      height: `${info.height} m`,
+      weight: `${info.weight} kg`,
+      image: info.ThumbnailImage,
+    };
+  }
 
-  // getting a generation from webpage
-  const pokeGens = await fetch(`${process.env.POKEMON_GENS}${info.slug}`);
-  const htmlGens = await pokeGens.text();
-  const parsedGens = new jsdom.JSDOM(htmlGens);
+  async function getGenAndDesc(attr) {
+    const response = await api.getPokemonSpeciesByName(attr);
+    const list = response.flavor_text_entries;
+    const match = list.filter(
+      (obj) =>
+        obj.language.name === 'en' &&
+        (obj.version.name === 'x' || obj.version.name === 'blue' || obj.version.name === 'sword')
+    )[0];
+    return {
+      generation: response.generation.name,
+      description: match.flavor_text.replace(/\n/g, '').trim(),
+    };
+  }
 
-  return {
-    name: info.name,
-    id: info.id,
-    type: info.type.map((x) => `/${x}`).join(', '),
-    abilities: info.abilities.join(', '),
-    weakness: info.weakness.join(', '),
-    height: `${info.height} m`,
-    weight: `${info.weight} kg`,
-    image: info.ThumbnailImage,
-    generation: parsedGens.window.document.querySelectorAll('.pi-smart-data-value')[0].childNodes[0]
-      .textContent,
-    description: parsedDesc.window.document
-      .querySelector('.version-x')
-      .textContent.replace(/\n/g, '')
-      .trim(),
-  };
+  const promises = [getInfo(attr), getGenAndDesc(attr)];
+  return Promise.all(promises)
+    .then((responsesList) => responsesList.reduce((acc, val) => ({ ...acc, ...val })))
+    .catch((err) => err);
 }
 
-getPokemon(55)
-  .then((response) => {
-    console.log(response);
-  })
-  .catch((error) => {
-    console.log('There was an ERROR: ', error);
-  });
-
-// usage
-// <function>(param)
-//   .then((data) => {
-//     console.log(data);
+// for time test
+// const start = Date.now();
+// const r = getRandomPokemonId(1, 898);
+// console.log(r);
+// getPokemon(r)
+//   .then((response) => {
+//     const end = Date.now();
+//     const time = (end - start) / 1000;
+//     console.log(response, time);
 //     process.exit(0);
 //   })
-//   .catch(() => {
-//     console.log('err');
-//     process.exit(0);
+//   .catch((error) => {
+//     console.log('There was an ERROR: ', error);
 //   });
 
 module.exports = {
